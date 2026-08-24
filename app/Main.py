483f0728d7b1e0f58b0c45990516bbc78c27d36d
Mahlatse007 <mahlatsemakgati72@gmail.com>
2026-08-24 10:41:18 +0200

    Created and fastAPI and updated main.py and requirement.txt

--- app/Main.py
+++ app/Main.py
@@ -1,1 +1,17 @@
-print("Learnuh!")
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {
+        "message": "Welcome to LearnHub!"
+        }
+
+
+@app.get("/home")
+def about():
+    return FileResponse("app/index.html")
+# Run the application using the command below in your terminal
+# uvicorn app.Main:app --reload
