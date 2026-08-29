c6b27755cba572ad73059a20be1f4f233de13e00
Mahlatse007 <mahlatsemakgati72@gmail.com>
2026-08-29 13:07:33 +0200

    Updated database.py, models.py, .env (gitignore)

---
+++ app/Database.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+
+DB_HOST = os.getenv("learnhub_db_host")
+DB_PORT = os.getenv("learnhub_db_port")
+DB_NAME = os.getenv("learnhub_db_name")
+DB_USER = os.getenv("learnhub_db_user")
+DB_PASSWORD = os.getenv("learnhub_db_password") 
+
+#postgresql://USERNAME:PASSWORD@HOST:PORT/DATABASE
+DATABASE_URL = (
+    f"postgresql://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+Base = declarative_base()
+
+# Test the connection
+try:
+       with engine.connect() as connection:
+        print("Database connection successful!")
+except Exception as e:
+    print(f"Error connecting to the database: {e}")
--- app/Main.py
+++ app/Main.py
@@ -1,17 +1,22 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
+from app.database import engine, Base
+from app import models
+
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 @app.get("/")
-def home():
+def info():
     return {
         "message": "Welcome to LearnHub!"
         }
 
 
 @app.get("/home")
-def about():
+def home():
     return FileResponse("app/index.html")
+
 # Run the application using the command below in your terminal
 # uvicorn app.Main:app --reload
